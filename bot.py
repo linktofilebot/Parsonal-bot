c7d80e200199e491ea1b1f5b4767134f0579ee47
linktofilebot <ya21973961@gmail.com>
2026-01-21 21:49:14 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ import pymongo
 from telebot import types
 
 # --- ২. কনফিগারেশন (Configuration) ---
-BOT_TOKEN = '8348660690:AAEAQUDHJm5QTZv4YMr7DrvddYPvzQF0-Wk' 
+BOT_TOKEN = '8348660690:AAH84DwkNBfUOqoWcl3s2tRartTQZFqm4I0' 
 MONGO_URL = 'mongodb+srv://roxiw19528:roxiw19528@cluster0.vl508y4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 # --- ৩. ডাটাবেস কানেকশন (Database Connection) ---
